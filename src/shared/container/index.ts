c86833ab8205bacb20461e6c5dc6bd2e4dee524b
Pedro Tizatto <phtizatto@ucs.br>
2020-08-09 09:54:55 -0300

    #2

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,19 +1,10 @@
 import { container } from 'tsyringe';
 import '@modules/users/providers';
 import './providers';
-import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
-import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
 import UserTokensRepository from '@modules/users/infra/typeorm/repositories/UserTokensRepository';
-import INotificationsRepository from '@modules/notifications/repositories/INotificationsRepository';
-import NotificationsRepository from '@modules/notifications/infra/typeorm/repositories/NotificationsRepository';
-
-container.registerSingleton<IAppointmentsRepository>(
-  'AppointmentsRepository',
-  AppointmentsRepository,
-);
 
 container.registerSingleton<IUsersRepository>(
   'UsersRepository',
@@ -23,9 +14,4 @@ container.registerSingleton<IUsersRepository>(
 container.registerSingleton<IUserTokensRepository>(
   'UserTokensRepository',
   UserTokensRepository,
-);
-
-container.registerSingleton<INotificationsRepository>(
-  'NotificationsRepository',
-  NotificationsRepository,
 );
